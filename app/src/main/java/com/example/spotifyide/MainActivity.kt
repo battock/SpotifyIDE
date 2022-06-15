d16bbb1e3df293052ab52fce2a2aea1c8efe59c6
Stuart Woodland <smwoodland@hotmail.com>
2022-06-15 13:27:10 +0100

    removed viewmodels etc

--- app/src/main/java/com/example/spotifyide/MainActivity.kt
+++ app/src/main/java/com/example/spotifyide/MainActivity.kt
@@ -3,19 +3,10 @@ package com.example.spotifyide
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.spotifyide.ui.screens.MainScreen
 import com.example.spotifyide.ui.theme.SpotifyIDETheme
-import com.example.spotifyide.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,28 +14,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel: MainViewModel by viewModels()
-            val navController = rememberNavController()
-
             SpotifyIDETheme {
-                MainScreen(
-                    navController = navController,
-                    viewModel
-                )
+                MainScreen()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SpotifyIDETheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
